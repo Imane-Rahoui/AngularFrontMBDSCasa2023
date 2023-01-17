88e4b978d9c4f7894a25b0e71523a463c6b370f3
micbuffa <micbuffa@gmail.com>
2023-01-17 11:12:57 +0100

    mat list et list item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AssignmentsComponent } from './assignments/assignments.component';
 import { RenduDirective } from './shared/rendu.directive';
+import { AssignmentDetailComponent } from './assignments/assignment-detail/assignment-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AssignmentsComponent,
-    RenduDirective
+    RenduDirective,
+    AssignmentDetailComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
     BrowserAnimationsModule, MatButtonModule, MatIconModule,
-    MatDividerModule
+    MatDividerModule, MatInputModule, MatFormFieldModule,
+    MatDatepickerModule, MatNativeDateModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,27 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Assignment } from './assignment.model';
 
 @Component({
   selector: 'app-assignments',
   templateUrl: './assignments.component.html',
   styleUrls: ['./assignments.component.css']
 })
-export class AssignmentsComponent {
+export class AssignmentsComponent implements OnInit {
+  // champs de formulaire
+  nomDevoir="";
+  dateDeRendu?:Date=undefined;
 
-  assignments = [
+  ngOnInit() {
+    console.log("AVANT AFFICHAGE");
+/*
+    setTimeout(() => {
+      this.ajoutDesactive = false;
+    }, 3000)
+    */
+  }
+
+  assignments:Assignment[] = [
     {
       nom: "Devoir Angular de Mr Buffa",
-      dateDeRendu : "2022-01-25",
+      dateDeRendu : new Date("2022-01-25"),
       rendu : false
     },
     {
       nom: "Devoir R de Mr Pasquier",
-      dateDeRendu : "2022-02-26",
+      dateDeRendu : new Date("2022-02-26"),
       rendu : false
     },
     {
       nom: "Devoir MongoDB de Mr MOPOLO",
-      dateDeRendu : "2022-01-6",
+      dateDeRendu : new Date("2022-01-6"),
       rendu : true
     }
   ];
+
+  onSubmit(event:any) {
+    console.log(this.nomDevoir + " date de rendu : " + this.dateDeRendu);
+    if((!this.nomDevoir) || (!this.dateDeRendu)) return;
+
+    const newAssignment = new Assignment();
+    newAssignment.nom = this.nomDevoir;
+    newAssignment.dateDeRendu = this.dateDeRendu;
+    newAssignment.rendu = false;
+
+    this.assignments.push(newAssignment);
+
+  }
 }
